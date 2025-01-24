67b44206763fd06094e61e79da346bc49d04686e
qrewy <114613848+qrewy@users.noreply.github.com>
2025-01-24 20:59:40 +0400

    update
    added showing price

--- frontend/src/components/AmountInput.tsx
+++ frontend/src/components/AmountInput.tsx
@@ -1,5 +1,5 @@
-// src/components/AmountInput.tsx
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import axios from 'axios';
 
 interface AmountInputProps {
     amount: string;
@@ -8,31 +8,57 @@ interface AmountInputProps {
     onSubmit: () => void;
 }
 
-export const AmountInput: FC<AmountInputProps> = ({
-                                                      amount,
-                                                      setAmount,
-                                                      amountError,
-                                                      onSubmit
-                                                  }) => {
+export const AmountInput: FC<AmountInputProps> = ({ amount, setAmount, amountError, onSubmit }) => {
+    const [tonPrice, setTonPrice] = useState<number | null>(null);
+
+    useEffect(() => {
+        const fetchTonPrice = async () => {
+            try {
+                const res = await axios.get("https://tonapi.io/v2/rates?tokens=ton&currencies=usd");
+                setTonPrice(res.data.rates["TON"].prices["USD"]);
+            } catch (error) {
+                console.error('Failed to fetch TON price', error);
+            }
+        };
+        fetchTonPrice();
+    }, []);
+
     return (
         <div className="input-group">
             <label>Choose quantity of Telegram Stars</label>
-            <input
-                type="number"
-                min="50"
-                max="1000000"
-                placeholder="Enter amount from 50 to 1,000,000"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
-                onKeyDown={(e) => {
-                    if (e.key === 'Enter') {
-                        onSubmit();
-                    }
-                }}
-                onBlur={onSubmit}
-                className={amountError ? 'input-error' : ''}
-                style={amountError ? { backgroundColor: '#ff586333', border: '1px solid #ff5863' } : {}}
-            />
+            <div style={{ position: 'relative' }}>
+                <input
+                    type="number"
+                    min="50"
+                    max="1000000"
+                    placeholder="Enter amount from 50 to 1,000,000"
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            onSubmit();
+                        }
+                    }}
+                    onBlur={onSubmit}
+                    className={amountError ? 'input-error' : ''}
+                    style={amountError ? { backgroundColor: '#ff586333', border: '1px solid #ff5863' } : {}}
+                />
+                {amount && Number(amount) > 49 && tonPrice && (
+                    <p
+                        style={{
+                            position: 'absolute',
+                            fontFamily: 'TT Interfaces',
+                            top: '25%',
+                            opacity: 0.5,
+                            right: '10px',
+                            transform: 'translateY(-50%)',
+                            color: 'white',
+                        }}
+                    >
+                        ~ {((Number(amount) * 0.015) / tonPrice).toFixed(2)} TON ({(Number(amount) * 0.015).toFixed(2)} $)
+                    </p>
+                )}
+            </div>
         </div>
     );
 };
